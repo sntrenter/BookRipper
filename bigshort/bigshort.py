4418529e636229e5170e51a6679da5f4b0370808
Sam Trenter <sntrenter@gmail.com>
2022-11-18 23:12:16 -0600

    2 books

---
+++ bigshort/bigshort.py
@@ -0,0 +1,26 @@
+link = "https://ipaudio6.com/wp-content/uploads/BOOKAUDIO/Big%20Short/{}.mp3"
+import time
+import requests
+
+i = 1
+
+
+def pullmp3(link,filename):
+    mp3 = requests.get(link)
+    if mp3.status_code == 200:
+        with open(filename,'wb') as f:
+            print("saving mp3")
+            f.write(mp3.content)
+    else:
+        print("link doesn't exist")
+
+
+while i < 100:
+    if i < 10:
+        num = "0" + str(i)
+    else:
+        num = str(i)
+    #print(num)
+    print(link.format(num))
+    pullmp3(link.format(num),str(num) + ".mp3")
+    i += 1
